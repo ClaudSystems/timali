14be281c110a5bf1ec772877eb312dfd5d31ec8f
ClaudSystems <ClaudSystems@gmail.com>
2026-04-19 20:09:05 +0200

    configuracao da base de dados no docker, conexao com  abase de dados, inicio de criacao de domains

---
+++ app-timali/grails-app/domain/Entidade.groovy
@@ -0,0 +1,28 @@
+class Entidade {
+    BigInteger id
+    Boolean ativo = true
+    String nome
+    String classificacao
+    Date data_de_emissao
+    Date data_de_validade
+    Date data_de_nascimento
+    Boolean em_divida
+    String email
+    Date estado_civil
+    String genero
+    String local_de_trabalho
+    String nacionalidade
+    String arquivo_de_indentificao
+    String nuit
+    String numero_de_indentificao
+    String profissao
+    String residencia
+    String telefone
+    String telefone1
+    String telefone2
+    String tipo_de_indentificao
+    String tipo_de_pessoa
+    String codigo
+
+
+}
