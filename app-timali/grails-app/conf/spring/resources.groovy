8da6f5ebf6af1083b308618f18a5b7bb524d17ba
ClaudSystems <ClaudSystems@gmail.com>
2026-04-20 17:12:55 +0200

    configuracao de spring security, do gemini

--- app-timali/grails-app/conf/spring/resources.groovy
+++ app-timali/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,18 @@
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 // Place your Spring DSL code here
 beans = {
+    corsConfigurationSource(UrlBasedCorsConfigurationSource) {
+        CorsConfiguration config = new CorsConfiguration()
+        config.allowedOrigins = ['http://localhost:3000']
+        config.allowedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
+        config.allowedHeaders = ['Content-Type', 'Authorization', 'X-Requested-With']
+        config.allowCredentials = true
+        config.maxAge = 3600L
+        registerCorsConfiguration("/**", config)
+    }
+
+    corsFilter(CorsFilter, ref('corsConfigurationSource'))
 }
