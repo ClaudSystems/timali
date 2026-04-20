b858455d341d6622d09b9427311895a2aa23e4e0
ClaudSystems <ClaudSystems@gmail.com>
2026-04-20 21:11:25 +0200

    ainda estamos na batalha do sprint security cores

--- app-timali/grails-app/conf/spring/resources.groovy
+++ app-timali/grails-app/conf/spring/resources.groovy
@@ -1,20 +1,30 @@
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.core.Ordered
 
 // Place your Spring DSL code here
 beans = {
     corsConfigurationSource(UrlBasedCorsConfigurationSource) {
         CorsConfiguration config = new CorsConfiguration()
+        
+        // Configurações sugeridas
         config.allowedOrigins = ['http://localhost:3000']
         config.allowedMethods = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
-        config.allowedHeaders = ['Content-Type', 'Authorization', 'X-Requested-With']
+        config.allowedHeaders = ['Authorization', 'Content-Type', 'Accept', 'X-Requested-With']
         config.allowCredentials = true
         config.maxAge = 3600L
+        
         registerCorsConfiguration("/**", config)
     }
 
-    corsFilter(CorsFilter, ref('corsConfigurationSource')) {
-        order = -100
+    // Filtro CORS com prioridade máxima
+    corsFilter(CorsFilter, ref('corsConfigurationSource'))
+
+    corsFilterRegistration(FilterRegistrationBean) {
+        filter = ref('corsFilter')
+        urlPatterns = ['/*']
+        order = Ordered.HIGHEST_PRECEDENCE // Garante que corre antes do Spring Security
     }
 }
